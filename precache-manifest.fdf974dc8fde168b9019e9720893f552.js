2dd09fde9e7993f84f33846105107fdb939b4145
olesyamogil <olesya.mogil@gmail.com>
2019-10-29 16:44:31 +0200

    Updates

---
+++ precache-manifest.fdf974dc8fde168b9019e9720893f552.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebb21a1cc83a5d367d23909b1534247",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "660f7ed1332c42d13be6",
+    "url": "/react_tabs/static/css/main.f4be10bc.chunk.css"
+  },
+  {
+    "revision": "f730d0c4bbf0eeb965aa",
+    "url": "/react_tabs/static/js/2.a717a1b1.chunk.js"
+  },
+  {
+    "revision": "660f7ed1332c42d13be6",
+    "url": "/react_tabs/static/js/main.4fa6c3fc.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
